5daa2f9ceffaedec445722b7c0b0e0b86efab149
Kenny <106347478+kennyhml@users.noreply.github.com>
2024-10-23 21:11:37 +0200

    feat(curves): Add bezier curve implementation

--- include/interception/beziercurve.h
+++ include/interception/beziercurve.h
@@ -1,1 +1,22 @@
 #pragma once
+#include <vector>
+#include "core.h"
+
+namespace interception
+{
+    using bezier_curve_t = std::vector<point>;
+
+    struct bezier_curve_parameters final
+    {
+    public:
+        int32_t knots = 2;
+        int32_t points = 100;
+
+        float mean_distortion = 1.f;
+        float stdev_distortion = 1.f;
+        float distortion_frequency = 0.5f;
+    };
+
+    [[nodiscard]] bezier_curve_t generate_curve(const point& from, const point& to,
+                                                const bezier_curve_parameters& = {});
+}
